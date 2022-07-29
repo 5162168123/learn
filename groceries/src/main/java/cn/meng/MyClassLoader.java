d9a28a4d3333b4a02fe42097fc6ecca2a69295a9
m5162168123 <364394606@qq.com>
2022-07-29 17:44:04 +0800

    daylly

---
+++ groceries/src/main/java/cn/meng/MyClassLoader.java
@@ -0,0 +1,12 @@
+package cn.meng;
+
+public class MyClassLoader extends ClassLoader {
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException{
+        Integer a = 1;
+        if(a instanceof Integer){
+
+        }
+    }
+}
