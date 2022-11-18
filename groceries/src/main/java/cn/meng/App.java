644720602dba8b9bafa1386ab2b844e6ad89d0df
15724737429 <haikeyi123>
2022-11-18 15:48:03 +0800

    daily

--- groceries/src/main/java/cn/meng/App.java
+++ groceries/src/main/java/cn/meng/App.java
@@ -1,7 +1,14 @@
 package cn.meng;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 
 /**
  * Hello world!
@@ -9,10 +16,10 @@ import java.util.Map;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        App app = new App();
-        new Thread(app::test);
+    static volatile AtomicInteger a = new AtomicInteger(0);
+    public static void main( String[] args ) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
+        System.out.println(simpleDateFormat.format(new Date(System.currentTimeMillis())).toString());
     }
     public void test(){
         System.out.println(123);
