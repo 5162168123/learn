b82d76511a6250652c463a1e166b95aae6bf940d
5162168123 <364394606@qq.com>
2023-06-03 17:25:05 +0800

    dayly

--- groceries/src/test/java/cn/meng/InterfaceTest.java
+++ groceries/src/test/java/cn/meng/InterfaceTest.java
@@ -1,10 +1,23 @@
 package cn.meng;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class InterfaceTest {
     @Test
-    void test01(){
+    public void test01(){
        TestInterface.DEFAULT.test();
+    }
+
+    @Test
+    public void test02(){
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i & 7);
+        }
     }
 }
